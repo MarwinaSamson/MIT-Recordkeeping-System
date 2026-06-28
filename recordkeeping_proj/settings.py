44305360fb0f023e4942b39cbc9aee1cde057ac4
HiitsMehayst <eh202201320@wmsu.edu.ph>
2026-06-28 22:52:04 +0800

    update
    update settings csrf

--- recordkeeping_proj/settings.py
+++ recordkeeping_proj/settings.py
@@ -27,6 +27,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000', cast=Csv())
+
 
 # Application definition
 
